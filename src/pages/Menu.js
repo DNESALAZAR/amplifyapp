de628ae2a0a54bffabad6167efac4188152f82c4
dsalazar <daniel.salazar@cobiscorp.com>
2021-07-19 16:37:07 -0500

    se añade login

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+
 class Menu extends Component {
 
-
+    cerrarSesion=()=>{
+        cookies.remove('id', {path: "/", secure: true, sameSite: 'none'})
+        cookies.remove('apellido_paterno', {path: "/", secure: true, sameSite: 'none'})
+        cookies.remove('apellido_pmterno', {path: "/", secure: true, sameSite: 'none'})
+        cookies.remove('nombre', {path: "/", secure: true, sameSite: 'none'})
+        cookies.remove('username', {path: "/", secure: true, sameSite: 'none'})
+        window.location.href='./';
+    }
+    componentDidMount(){
+        if(!cookies.get('username')){
+            window.location.href="./";
+        }
+    }
     render() {
+        console.log('id: ' + cookies.get('id'));
+        console.log('apellido_paterno: ' + cookies.get('apellido_paterno'));
+        console.log('apellido_materno: ' + cookies.get('apellido_materno'));
+        console.log('nombre: ' + cookies.get('nombre'));
+        console.log('username: ' + cookies.get('username'));
         return (
             <div>
                 Menu Principal
+
                 <br />
+                <button onClick={()=>this.cerrarSesion()}>Cerrar Sesión</button>
             </div>
         );
     }
 }
 
-export default withAuthenticator(Menu, { includeGreetings: true });
+export default Menu;
