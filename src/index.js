9e6eb4b6eb9382ab894049b1467fca23c3b2c47a
dsalazar <daniel.salazar@cobiscorp.com>
2021-07-19 12:11:23 -0500

    se añade withAuthenticator

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes/Routes';
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+Amplify.configure(config);
 
 
 ReactDOM.render(
